4f88bbd1495b6361957ab29637fcd7d74b37920b
msakac <msakac98@gmaIl.com>
2024-05-21 13:18:24 +0200

    new

--- pylint_nivels_git/commands.py
+++ pylint_nivels_git/commands.py
@@ -1,21 +1,49 @@
 import argparse
 import os
 
-def run_pylint_nivels(pylint_configuration):
+# Dictionary mapping keys to configuration file paths
+CONFIGURATION_PATHS = {
+    'path_to_pylintrc': 'configurations/.pylintrc',
+    'path_to_easy_peasy': 'configurations/.pylintrc_easy_peasy',
+    # Add more configuration paths as needed
+}
+
+import os
+
+def run_pylint_nivels(pylint_configuration_key):
+    # Check if the provided key exists in the configuration paths dictionary
+    if pylint_configuration_key not in CONFIGURATION_PATHS:
+        print(f"Error: Configuration key '{pylint_configuration_key}' not found.")
+        return
+
+    # Get the corresponding configuration file path
+    pylint_configuration = CONFIGURATION_PATHS[pylint_configuration_key]
+
+    # Get the current directory of the script
+    current_directory = os.path.dirname(__file__)
+
+    # Form the absolute path to the configuration file
+    configuration_path = os.path.join(current_directory, pylint_configuration)
+
+    # Check if the configuration file exists
+    if not os.path.exists(configuration_path):
+        print(f"Error: Configuration file '{configuration_path}' not found.")
+        return
+
     git_files = os.popen("git diff --name-only --diff-filter=ACMRTUXB | grep  -E '.py$'").read()
-    print("Git files" + git_files)
     if not git_files:
         exit(0)
 
-    pylint_command = f"pylint --load-plugins=pylint_odoo --rcfile=configurations/{pylint_configuration} {git_files} --output-format=colorized"
+    # Use the retrieved configuration file path in the pylint command
+    pylint_command = f"pylint --load-plugins=pylint_odoo --rcfile={configuration_path} {git_files} --output-format=colorized"
     os.system(pylint_command)
 
+
 def main():
-    print('hej im main')
     parser = argparse.ArgumentParser(description="Run pylint with custom configuration")
-    parser.add_argument("pylint_configuration", help="Path to the pylint configuration file")
+    parser.add_argument("pylint_configuration_key", help="Key to retrieve the pylint configuration file path")
     args = parser.parse_args()
-    run_pylint_nivels(args.pylint_configuration)
+    run_pylint_nivels(args.pylint_configuration_key)
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="pylint_nivels_git",
-    version="0.1",
+    version='1.0',
     packages=find_packages(),
+    package_data={'': ['configurations/*']},
+    include_package_data=True,
     entry_points={
         "console_scripts": [
             "pylint_nivels=pylint_nivels_git.commands:main",
